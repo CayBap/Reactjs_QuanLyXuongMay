b2d8c533c2e1c182839c6351b3d57df9fced4bbb
thangdd <youci.thang@gmail.com>
2019-05-11 07:17:54 +0700

    update

--- src/constants/urlRequrest.js
+++ src/constants/urlRequrest.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://localhost:4040';
+// export const BASE_URL = 'http://192.168.1.10:4040';
 
 export const LOGIN = `${BASE_URL}/api/auth/login`;
 export const REGISTER = `${BASE_URL}/api/auth/register`;
